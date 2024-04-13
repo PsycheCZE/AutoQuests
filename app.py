02f0d5d0358677bc51dd5fb1e0464492ab0c0173
PsycheCZE <163991267+PsycheCZE@users.noreply.github.com>
2024-04-13 09:48:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,15 +3,18 @@ import requests
 import time
 from threading import Thread, Event
 from ahk import AHK
-from tkinter import Tk, Button, Label, Radiobutton, IntVar, Frame
+from tkinter import Tk, Button, Label, Radiobutton, IntVar, Frame, Entry
 from pynput.mouse import Listener as MouseListener
 import keyboard 
+import webbrowser
+import threading
 
 ahk = AHK()
 
 selected_position_x = None
 selected_position_y = None
 is_selecting_position = False
+
 
 def on_click(x, y, button, pressed):
     global is_selecting_position, selected_position_x, selected_position_y
@@ -22,6 +25,15 @@ def on_click(x, y, button, pressed):
         
         position_text.config(text=f"Pozice= X: {x}, Y: {y}", font=("Helvetica", 10), bg='#0f0f0f', fg='white')  # Aktualizace textu tlačítka
         return False 
+        
+def get_screen_size():
+    monitors = get_monitors()
+    if monitors:
+        # Předpokládáme, že používáte hlavní monitor
+        return monitors[0].width, monitors[0].height
+    else:
+        return None, None  # Žádné monitory nebyly nalezeny
+
 
 def start_position_selection():
     global is_selecting_position
@@ -44,29 +56,29 @@ def execute_ahk_actions(text, remove_f=False, type_34=False):
     ahk.key_press('F')
     time.sleep(0.5)
 
-    if not is_pixel_white(1214, 250): #1. bílá plocha v inventáři (pro kontrolu zda je otevřen inventář)
+    if not is_pixel_white(1214, 250): #bílá plocha v inventáři (pro kontrolu zda je otevřen inventář)
         ahk.key_press('F')
         print("No White")
     
     if type_34:
         update_task_label("Consume Potions")
-        ahk.mouse_move(414, 457, speed=5) #2. Klik na ikonu potionů v inventáři
+        ahk.mouse_move(414, 457, speed=5) #Klik na ikonu potionů v inventáři
         time.sleep(0.1)
         ahk.click()
         time.sleep(0.1)
 
-        ahk.mouse_move(selected_position_x, selected_position_y, speed=5) #Klik na pozici IV potionů (neměnit)
+        ahk.mouse_move(selected_position_x, selected_position_y, speed=5) #Klik na pozici IV potionů
         for _ in range(200):
             ahk.click()
             time.sleep(0.001) 
         time.sleep(1)
     
     if not type_34:
-        ahk.mouse_move(413, 398, speed=5) #3. Klik na ikonu itemů v inventáři (batůžek)
+        ahk.mouse_move(413, 398, speed=5) #Klik na ikonu itemů v inventáři (batůžek)
         time.sleep(0.1)
         ahk.click()
 
-        ahk.mouse_move(1340, 251, speed=5) #4. Klik na search bar
+        ahk.mouse_move(1340, 251, speed=5) #Klik na search bar
         time.sleep(0.3)
         ahk.click()
         ahk.click()
@@ -77,23 +89,23 @@ def execute_ahk_actions(text, remove_f=False, type_34=False):
         ahk.type(text)
         time.sleep(0.1)
     
-        ahk.mouse_move(504, 381, speed=5) #5. Klik na první nalezený item 
+        ahk.mouse_move(504, 381, speed=5) #Klik na první nalezený item 
         time.sleep(0.1)
         ahk.click()
         time.sleep(0.2)
 
-        ahk.mouse_move(947, 746, speed=5) #6. Klik na tlačítko "ok", když se vypíše chyba, že už je v lokaci item
+        ahk.mouse_move(947, 746, speed=5) #Klik na tlačítko "ok", když se vypíše chyba, že už je v lokaci item
         time.sleep(0.1)
         ahk.click()
         time.sleep(0.2)
 
     if type_34:
-        ahk.mouse_move(410, 394, speed=10) #7. Klik na ikonu itemů v inventáři (batůžek)
+        ahk.mouse_move(410, 394, speed=10) #Klik na ikonu itemů v inventáři (batůžek)
         time.sleep(0.1)
         ahk.click()
         time.sleep(0.1)
 
-    ahk.mouse_move(908, 126, speed=10) #8. Klik mimo inventář před zavřením (kamkoliv nad okno inventáře)
+    ahk.mouse_move(908, 126, speed=10) #Klik mimo inventář před zavřením (kamkoliv nad okno inventáře)
     time.sleep(0.1)
     if not remove_f:
         ahk.key_press('F')
@@ -106,14 +118,98 @@ def stop_all_threads():
     thread_stop_events.clear()
     print("Threads cleared!")
 
+def click_research():
+    ahk.mouse_move(504, 381, speed=2) #Klik na první nalezený item 
+    time.sleep(0.1)
+    ahk.click()
+
+    ahk.mouse_move(947, 746, speed=2) #Klik na tlačítko "ok", když se vypíše chyba, že už je v lokaci item
+    time.sleep(0.1)
+    ahk.click()
+    time.sleep(0.2)
+
+    ahk.mouse_move(1340, 251, speed=2) #Klik na search bar
+    time.sleep(0.3)
+    ahk.click()
+    ahk.click()
+    time.sleep(0.1)
+
 def press_space_and_r():
-    update_task_label("ANTI AFK!")
+    update_task_label("Consumables!")
     print("Pressing Space and R")
+
+    ahk.key_press('F')
+    time.sleep(0.3)
+
+    ahk.mouse_move(413, 398, speed=3) #Klik na ikonu itemů v inventáři (batůžek)
+    time.sleep(0.1)
+    ahk.click()
+    time.sleep(0.3)
+
+    if not is_pixel_white(1214, 250): #bílá plocha v inventáři (pro kontrolu zda je otevřen inventář)
+        ahk.key_press('F')
+        print("No White")
+    
+    ahk.mouse_move(1340, 251, speed=5) #Klik na search bar
+    time.sleep(0.3)
+    ahk.click()
+    ahk.click()
+    time.sleep(0.1)
+    
+    ahk.type("Rainbow Fruit")
+    time.sleep(0.1)
+
+    click_research()
+
+    ahk.type("Apple")
+    time.sleep(0.1)
+
+    click_research()
+
+    ahk.type("Pineapple")
+    time.sleep(0.1)
+
+    click_research()
+
+    ahk.type("Banana")
+    time.sleep(0.1)
+
+    click_research()
+
+    ahk.type("Orange")
+    time.sleep(0.1)
+
+    click_research()
+
+    ahk.type("Watermelon")
+    time.sleep(0.1)
+
+    click_research()
+
+    ahk.type(flag_name_entry.get())
+    time.sleep(0.1)
+
+    click_research()
+
+    ahk.type("Sprinkler")
+    time.sleep(0.1)
+
+    ahk.mouse_move(504, 381, speed=1) #Klik na první nalezený item 
+    time.sleep(0.1)
+    ahk.click()
+
+    ahk.mouse_move(947, 746, speed=1) #Klik na tlačítko "ok", když se vypíše chyba, že už je v lokaci item
+    time.sleep(0.1)
+    ahk.click()
+    time.sleep(0.2)
+
+    ahk.key_press('F')
+    time.sleep(0.1)
     ahk.key_press('Space')
     time.sleep(0.1)
     ahk.key_press('R')
     time.sleep(0.1)
-    ahk.mouse_move(51, 701, speed=5) #9. Klik do pryč
+    ahk.mouse_move(51, 746, speed=5)
     time.sleep(0.1)
     ahk.click()
 
@@ -152,7 +248,7 @@ def activate_goal(goal_type):
         type_34 = False
     else: 
         text = "ANTI AFK"
-        interval = 60
+        interval = 1
         remove_f = False
         type_34 = False 
         
@@ -172,6 +268,7 @@ def execute_ahk_script(text, interval, remove_f=False, type_34=False, stop_event
 
 running_threads = []
 thread_stop_events = []
+
 
 def check_api_and_run():
     global active_goal_type
@@ -197,9 +294,10 @@ def check_api_and_run():
             stop_all_threads()
             print("No active priority goal type, pressing space and R.")
             press_space_and_r()
-            time.sleep(60)
+            time.sleep(1)
 
         time.sleep(5)
+
 
 def toggle_script():
     global running_threads, thread_stop_events, active_goal_type
@@ -228,7 +326,7 @@ stop_thread = Event()
 root = Tk()
 root.title("Auto Clan Quests | by Psyche")
 root.attributes('-topmost', True)
-root.geometry('280x225')
+root.geometry('280x300')
 
 root.configure(bg='#333333')
 
@@ -242,7 +340,7 @@ current_task_label = Label(root, text="Aktuální úloha: ", font=("Helvetica", 
 current_task_label.pack(pady=(0, 0))
 
 radiobutton_frame = Frame(root, bg='#333333')
-radiobutton_frame.pack(pady=5)
+radiobutton_frame.pack(pady=0)
 
 clan_choice = IntVar(value=1)
 
@@ -252,9 +350,13 @@ Radiobutton(radiobutton_frame, text="VLP2", font=("Helvetica", 10), variable=cla
 position_frame = Frame(root, bg='#0f0f0f') 
 position_frame.pack(pady=2, anchor='center')  
 
+flag_name_entry_text = Label(root, text="Název vlajky: ", font=("Helvetica", 11), bg='#333333', fg='white')
+flag_name_entry_text.pack(pady=(0, 0))
+flag_name_entry = Entry(root, font=('Helvetica', 10), bg='white', fg='black')
+flag_name_entry.pack(pady=(0, 20))
+
 position_text = Label(root, text="", font=("Helvetica", 10), bg='#333333')
 position_text.pack(pady=(0, 0))
-
 
 button_frame = Frame(root, bg='#333333') 
 button_frame.pack(pady=10, anchor='center')  
@@ -262,14 +364,14 @@ button_frame.pack(pady=10, anchor='center')
 pick_position_button = Button(button_frame, text="Vybrat pozici\nIV potky", command=start_position_selection, 
                               bg='#333333', fg='white', 
                               font=('Helvetica', 10, 'bold'), 
-                              padx=10, pady=10, 
+                              padx=10, pady=0, 
                               borderwidth=2, relief="ridge")
 pick_position_button.pack(side='left', padx=10) 
 
 button = Button(button_frame, text="START (F7)", command=toggle_script, 
                 bg='#556677', fg='white', 
                 font=('Helvetica', 10, 'bold'), 
-                padx=10, pady=10, 
+                padx=10, pady=6, 
                 borderwidth=2, relief="ridge")
 button.pack(side='left') 
 
